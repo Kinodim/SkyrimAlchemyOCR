698b8095787ec32aa2d0a901fb28cb3d2771ac0d
Dominik Schulz <d.dominik.schulz@gmail.com>
2017-02-14 01:27:52 +0100

    Add select recipe loop to main

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 const string dictionary_file = "../data/dictionary.json";
 const string recipes_file = "../data/recipes.json";
 const string default_image_file = "../images/sample.jpg";
+const string output_file = "../data/selected_recipes.json";
 const int OCR_matching_offset = 1;
 const double OCR_matching_length_weight = 0.5;
 
@@ -32,6 +33,18 @@ void ProcessImage(const string& filename, Inventory& inventory){
 			continue;
 		}
 		inventory.AddItem(entry.first, entry.second);
+	}
+}
+
+void ShowRecipes(const vector<Recipe>& recipes){
+	if(recipes.empty()){
+		cout << "No recipes found..." << endl;
+		return;
+	}
+	int recipe_id = 1;
+	for(const auto& r : recipes){
+		cout << "#" << recipe_id << ": " << r << endl;
+		recipe_id++;
 	}
 }
 
@@ -67,14 +80,56 @@ int main(int argc, char *argv[])
 	vector<Recipe> possible_recipes;
 	RecipeSearcher searcher;
 	searcher.GetPossibleRecipes(inventory, possible_recipes);
+	ShowRecipes(possible_recipes);
 
-	if(possible_recipes.empty()){
-		cout << "No recipes found..." << endl;
+	vector<Recipe> selected_recipes;
+	while(true){
+		string input;
+		cout << "Selected Recipe ID (q to quit): ";
+		getline(cin, input);
+		if(input == "q"){
+			break;
+		}
+
+		int selected_id = 0;
+		try{
+			selected_id = stoi(input);
+		}
+		catch(invalid_argument){
+			cout << "Invalid input." << endl;
+			continue;
+		}
+		if(selected_id < 1 || selected_id > possible_recipes.size()){
+			cout << "Invalid index." << endl;
+			continue;
+		}
+
+		selected_recipes.push_back(possible_recipes[selected_id - 1]);
+		for(const auto& i : possible_recipes[selected_id - 1].ingredients){
+			inventory.RemoveItem(i);
+		}
+		cout << string(20, '-') << endl;
+		cout << "Selected Recipes:" << endl;
+		ShowRecipes(selected_recipes);
+
+		searcher.GetPossibleRecipes(inventory, possible_recipes);
+		cout << string(20, '-') << endl;
+		cout << "Possible Recipes:" << endl;
+		ShowRecipes(possible_recipes);
+	}
+
+	if(selected_recipes.empty()){
 		return 0;
 	}
-	for(const auto& r : possible_recipes){
-		cout << r << endl;
+	ofstream outstream(output_file);
+	if(!outstream){
+		cout << "Error opening output file " << output_file << endl;
 	}
+	json j = json::array();
+	for(const auto& r : selected_recipes){
+		j.push_back(r);
+	}
+	outstream << setw(4) << j;
 
 	return 0;
 }
